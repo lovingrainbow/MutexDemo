54296aeb9700294a1bcb4fd564357db7a2333468
lovingrainbow <lwydctl@gmail.com>
2019-02-18 15:20:55 +0800

    Mutex program finish.

--- MutexDemo/Program.cs
+++ MutexDemo/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MutexDemo
 {
     class Program
     {
+        static int seq = 0;
+        static Mutex mut = new Mutex();
+
         static void Main(string[] args)
         {
+            for (int i = 0; i < 10; i++)
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    MThread();
+                });
+            }
+            Console.Read();
+        }
+        static void MThread()
+        {
+            int seqId = ++seq;
+            //進入保護區間
+            mut.WaitOne();
+            Console.WriteLine(seqId + "進入MThread Mutex.");
+
+            Thread.Sleep(100);
+
+            Console.WriteLine(seqId + "離開MThread Mutex.");
+            //離開保護區間
+            mut.ReleaseMutex();
         }
     }
 }
